257847e793b0650052542766b951e77b6f475cce
ROHIT <2401104114cse@gcekjr.ac.in>
2025-11-29 00:31:26 +0530

    mgg

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,12 @@
-const UseFetch = (url) => {
-    
+import React, { useEffect, useState } from 'react'
+let url='https://api.npoint.io/4459a9a10e43812e1152';
+ const useFetch = (url) => {
+    const[data,setData]=useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+  return [data]
 }
 
-export default UseFetch
+export default useFetch
